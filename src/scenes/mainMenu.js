5e2f6a8482324aff907b358a9a2e6165b7619df0
casalaspro <casalaspro.alessandro@gmail.com>
2024-10-27 19:32:00 +0100

    ADD - platforms game object and onUpdate bacground movement

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -12,8 +12,25 @@ export default function mainMenu(){
     k.add([k.sprite("chemical-bg"), k.pos(0, 0), k.scale(2), k.opacity(0.8)]),
     // i placed the x position of the second image *2 because the two images are scaled to the double and the second image has to follow the first image to create the illusion of infinite scrolling
     k.add([k.sprite("chemical-bg"), k.pos(bgPieceWidth * 2, 0), k.scale(2), k.opacity(0.8)]),
+  ];
 
-  ]
+  const platformWidth = 1280;
+  const platforms = [
+    k.add([k.sprite("platforms"), k.pos(0, 450), k.scale(4)]),
+    k.add([k.sprite("platforms"), k.pos(platformWidth * 4, 450), k.scale(4)]),
+  ];
 
+  // this function will run each update. It depends on fps
+  k.onUpdate(() => {
+    if(bgPieces[1].pos.x < 0){
+      bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
+      // i use the shift() method to extract the 0 index from the array and with push() i place it last
+      bgPieces.push(bgPieces.shift());
+    }
+
+     // each update .move() function moves the game object
+     bgPieces[0].move(-100, 0);
+     bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
+  })
 
 }
